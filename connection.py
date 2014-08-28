16c5c5be12d973cb37e427353c9a663a65af1e29
coral <weilandeshanhuhai@126.com>
2014-08-28 16:28:51 +0800

    connection.py

--- connection.py
+++ connection.py
@@ -47,7 +47,7 @@ def agent_auth(transport, username):
 
 
 def manual_auth(username, hostname):
-    try:
+    if os.path.isfile(os.path.join(os.environ['HOME'], '.ssh', 'id_rsa')):
         default_path = os.path.join(os.environ['HOME'], '.ssh', 'id_rsa')
         path = default_path
         try:
@@ -56,7 +56,7 @@ def manual_auth(username, hostname):
             password = getpass.getpass('RSA key password: ')
             key = paramiko.RSAKey.from_private_key_file(path, password)
         t.auth_publickey(username, key)
-    except IOError:
+    elif os.path.isfile(os.path.join(os.environ['HOME'], '.ssh', 'id_dsa')):
         default_path = os.path.join(os.environ['HOME'], '.ssh', 'id_dsa')
         path = default_path
         try:
@@ -66,7 +66,7 @@ def manual_auth(username, hostname):
             key = paramiko.DSSKey.from_private_key_file(path, password)
         t.auth_publickey(username, key)
     #except paramiko.AuthenticationException as e:
-    except:
+    else:
         pw = getpass.getpass('Password for %s@%s: ' % (username, hostname))
         try:
             t.auth_password(username, pw)
